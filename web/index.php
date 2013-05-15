d4e3140e55dc819be1820b51f20a8911dbbc4441
Robbin <86378@ict-idcollege.nl>
2013-05-15 16:37:23 +0200

    Added first session test in the index file

--- web/index.php
+++ web/index.php
@@ -6,22 +6,47 @@ require_once __DIR__ . '/../includes/player.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+// start session
+$session = new Session();
+$session->start();
 
 // routes
-$app->get('/', function() {
+$app->get('/', function() use ($app, $session) {
 	// deck
-	$deck = new Deck();
-	$deck->shuffle();
+	if(!$session->get('started')) { // no game started yet
+		// session init
+		echo 'new session';
+		$session->set('started', true);
 
-	echo '<p>My cards:</p>';
+		// create deck
+		$deck = new Deck();
+		$session->set('deck_obj', $deck);
+		$deck->shuffle();
 
-	$player1 = new Player();
-	$player1->addCards($deck->giveCards(7));
-	$player1->renderCards();	
+		// create player
+		$player = new Player();
+		$session->set('player_obj', $player);
+		$player->addCards($deck->giveCards(7));
 
-	echo '<p>Deck has currently ' . $deck->countCards() . ' cards left</p>';
+		// render cards
+		echo '<p>My cards:</p>';
+		$player->renderCards();	
+	} else { // game already started
+		$player = $session->get('player_obj');
+		$player->renderCards();	
+		$session->invalidate();
+		//echo '<p>Deck has currently ' . $deck->countCards() . ' cards left</p>';
+	}
+
+	return new Response();
 });
 
 // uitvoeren
